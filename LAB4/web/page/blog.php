95ed148a19447b7a57de90f1cee0f177cfac0fa2
unholy164-glitch <unholy164@gmail.com>
2025-12-16 15:12:04 +0700

    LAB5

---
+++ LAB4/web/page/blog.php
@@ -0,0 +1,11 @@
+<?php
+echo "I LOVE IT";
+
+$name = "jo";
+$age = 23;
+
+
+$arr = [1, 2, "test"];
+var_dump($arr[2]);
+?>
+<?= $name . " " . $age ?>
---
+++ LAB4/web/page/php/condb.php
@@ -0,0 +1,21 @@
+<?php
+$host = "db";
+$user = "root";
+$pass = "1234";
+$db = "mydb";
+
+try{
+    $condb = new mysqli($host, $user, $pass, $db);
+
+    if($condb->connect_error){
+        throw new Exception("Connection failed: " );
+
+    }
+}
+catch(Exception $e){
+    echo $e->getMessage();
+}
+
+
+
+$condb = new mysqli($host, $user, $pass, $db);
